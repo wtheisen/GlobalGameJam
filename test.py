150db178187e34b34901c5c8dbbd4ec3b90e3b56
wtheisen <wtheisen@nd.edu>
2018-01-26 22:06:21 -0500

    tried something for dynamic imports

--- test.py
+++ test.py
@@ -27,25 +27,25 @@ i=0
 
 
 class PlayerSprite(pygame.sprite.Sprite):
-	MAX_FORWARD_SPEED = 10
-	MAX_REVERSE_SPEED = 10
-	ACCERATION = 2
-	TURN_SPEED = 5
-	
-	def __init__(self, image, position):
-		pygame.sprite.Sprite.__init__(self)
-		self.src_image = pygame.image.load(image)
-		self.position = position
-		self.speed = self.direction = 0
+        MAX_FORWARD_SPEED = 10
+        MAX_REVERSE_SPEED = 10
+        ACCERATION = 2
+        TURN_SPEED = 5
+
+        def __init__(self, image, position):
+                pygame.sprite.Sprite.__init__(self)
+                self.src_image = pygame.image.load(image)
+                self.position = position
+                self.speed = self.direction = 0
 
 
-	
-	def update(self, deltat):
-		x, y = self.position	
-		self.position = (x, y)
-		self.image = pygame.transform.rotate(self.src_image, self.direction)	
-		self.rect=self.image.get_rect()
-		self.rect.center = self.position
+
+        def update(self, deltat):
+                x, y = self.position
+                self.position = (x, y)
+                self.image = pygame.transform.rotate(self.src_image, self.direction)
+                self.rect=self.image.get_rect()
+                self.rect.center = self.position
 
 
 import sheetParser
@@ -53,9 +53,9 @@ sheetParser.readSheets()
 
 classFolder = "./classes/"
 for filename in os.listdir(classFolder):
-	if filename.endswith('.py'):
-		importlib.import_module(os.path.join(classFolder, filename))
- 
+        if filename.endswith('.py'):
+                importlib.import_module(os.path.join(classFolder, filename), package=filename)
+
 rect = screen.get_rect()
 
 player = PlayerSprite('alien.png', rect.center)
@@ -63,37 +63,37 @@ player_group = pygame.sprite.RenderPlain(player)
 
 
 while 1:
-	#aksjfl;aksjd
-	clock.tick(FRAMES_PER_SECOND)
-	for event in pygame.event.get():		
-		if event.type == KEYDOWN:
-			if event.key == K_w:
-				player.position = (0,0)
-				## move character
-						
+        #aksjfl;aksjd
+        clock.tick(FRAMES_PER_SECOND)
+        for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                        if event.key == K_w:
+                                player.position = (0,0)
+                                ## move character
 
-		if event.type == 12:
-			sys.exit(0)
 
-		print (event)
-	screen.fill((0,0,0))
-	keypressed = pygame.key.get_pressed()
-	if keypressed[pygame.K_ESCAPE]:
-		print (keypressed)
+                if event.type == 12:
+                        sys.exit(0)
 
-	i+=1
-	x=0
-	y=0
-	##rotated = pygame.transform.rotate(alien, 45* math.pi / 180)
-	##screen.blit(rotated ,(i, 100)) 
-	player_group.update(deltat)
-	player_group.draw(screen)
+                print (event)
+        screen.fill((0,0,0))
+        keypressed = pygame.key.get_pressed()
+        if keypressed[pygame.K_ESCAPE]:
+                print (keypressed)
 
-	pygame.display.flip()
+        i+=1
+        x=0
+        y=0
+        ##rotated = pygame.transform.rotate(alien, 45* math.pi / 180)
+        ##screen.blit(rotated ,(i, 100))
+        player_group.update(deltat)
+        player_group.draw(screen)
 
-	pygame.event.pump()	
-	
-	
+        pygame.display.flip()
+
+        pygame.event.pump()
+
+
 
 
 #screen = pygame.display.set_mode((1024,768),FULLSCREEN)
